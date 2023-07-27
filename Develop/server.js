04e55549a12413ebb2dab3b68bb825b2eac7f136
Krysta Gonzalez <gnosisknows28@gmail.com>
2023-07-27 12:50:06 -0500

    updating server.js

---
+++ Develop/server.js
@@ -0,0 +1,19 @@
+//Import Express
+const express = require('express');
+//Import 'path'(built in Node.JS package) to resolve path of giles that are located on the server. 
+const path = require('path');
+//Require 'fs' to READ and WRITE to the 'FileSystem'
+const fs = require('fs');
+//Initialize instance of Express
+const app=express();
+//===========================================//
+
+// Set the port of our application//process.env.PORT means: whatever is in the environment variable PORT, or 3001 if there's nothing there.
+const PORT = process.env.PORT || 3001;
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//Static middleware pointing to the public folder
+app.use(express.static('public'));
+
