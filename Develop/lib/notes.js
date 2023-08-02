25edabc6ad5cd99e8c2e87e535a9761d3881374c
Krysta Gonzalez <gnosisknows28@gmail.com>
2023-08-02 10:06:19 -0500

    added lib folder with notes file

---
+++ Develop/lib/notes.js
@@ -0,0 +1,4 @@
+//HERE is where we will utilize fs and path 
+const fs = require('fs');
+const path = require('path');
+
--- Develop/server.js
+++ Develop/server.js
@@ -15,9 +15,12 @@ const htmlRoutes = require('./routes/htmlRoutes');
 app.use(express.json());
 //This function "express.urlencoded()" is used to parse the request body into a JavaScript object. The `extended` option is set to true, which means that the function will parse the request body using the extended parsing rules. This will allow the function to parse more complex request bodies.
 app.use(express.urlencoded({ extended: true }))
-
 //Static middleware pointing to the public folder
 app.use(express.static('public'));
+//This code tells the application to exicute the 'apiRoutes' function for each erquest that is made to the './api' route.
+app.use('./api', apiRoutes);
+//This code tells the applucation to execute the 'hrmlRoutes' function for each request that is made to the root route('/').
+app.use('/', htmlRoutes) 
 
 
 // listen() method is responsible for listening for incoming connections on the specified port 
