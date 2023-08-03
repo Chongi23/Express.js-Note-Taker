060d78fb783a9eee0266fd409432a5ba6147c066
Krysta Gonzalez <gnosisknows28@gmail.com>
2023-08-02 22:22:20 -0500

    Updating README

--- Develope/lib/notes.js
+++ Develope/lib/notes.js
@@ -8,28 +8,26 @@ function createNewNote(body, notesArray) {
   const newNote = body
   //This code adds notes to the notesArray
   notesArray.push(newNote);
-     //fs.writeFileSync() method is used to write data synchronously. 
     //The "path.join()" methos is used to create a file path that points to the 'db.json' file in the 'db' directory.
     //__dirName variable is used to get the current directory path.
-    //The 'db.json' file is created if it does not exist already.
     //JSON.stringify() method is used to convert the 'notesArray' object to a JSON string,
-    //the resulting JSON string is then written to the 'db.json' file using the 'fs.writeFileSync() method
-  fs.writeFileSync(
-    path.join(__dirname, "../db/db.json"),
-    JSON.stringify({notes: notesArray}, null, 2)
-  );
+    //the resulting JSON string is then written to the 'db.json' file using the 'fs.writeFileSync() method\
+    
+  fs.writeFileSync
+  (path.join(__dirname, "../db/db.json"),JSON.stringify({notes: notesArray}, null, 2));
   return newNote;
 };
 
 
-//Function to update Notes
+//Function to update(DELETE) Notes
 function updateDb(id, notesArray) {
     const deletedNote = id;
     for (let i = 0; i < notesArray.length; i++) {
       if (deletedNote === notesArray[i].id) {
         //The splice() method is used to add or remove elements from an array. In this task, we are using the splice() method to remove the element at index i from the array notesArray.
        notesArray.splice(i, 1);
-        fs.writeFileSync(
+
+        fs.writeFile(
           path.join(__dirname, "../db/db.json"),
           JSON.stringify({notes: notesArray}, null, 2), err => {
             if (err) {
