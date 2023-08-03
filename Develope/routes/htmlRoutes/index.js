cbca7563ea6dfcef28dc56d3a142d81c04f53ab6
Krysta Gonzalez <gnosisknows28@gmail.com>
2023-08-02 21:43:49 -0500

    adding notes to functionality of code and making sure paths are correct

--- Develope/routes/htmlRoutes/index.js
+++ Develope/routes/htmlRoutes/index.js
@@ -1,16 +1,25 @@
 const path = require("path");
+//This creates a router object in JS,this function takes no arguments and returns a router object.
 const router = require('express').Router();
 
+//Here a GET route for the root URL it set up using router.get() method.
+//It then sends the HTML file for the home page as a response using the res.sendFile() method.
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
+
+//This function sets up a GET route for the /notes URL of the website using the router.get() method
+//It then sends the HTML file for the notes page as a response using the rex.sendFile() method.
 
 router.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "../../public/notes.html"));
 });
 
-router.get("*", (req, res) => {
+//This function sets up a GET route for all URLs using the router.get() method.
+//It then sends the HTML file for the index.html page as a response using the res.sendFile() method.
+router.get("*", (req, res) => { 
   res.sendFile(path.join(__dirname, "../../public/index.html"));
 });
 
+//Exports 'router'.
 module.exports = router;
